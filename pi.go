14ae8f45589ca5c55769d522c8f0ff97ae6a0246
Peter Olsen <pto@me.com>
2016-12-30 18:32:25 -0500

    add profiling and performance improvements

--- pi.go
+++ pi.go
@@ -2,15 +2,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"math/big"
 	"os"
 	"path/filepath"
+	"runtime/pprof"
 	"strconv"
 	"time"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write CPU profile to a file")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	start := time.Now()
 	digits := digitsFromCommandLine(1000)
 	scaledPi := fmt.Sprint(π(digits))
@@ -19,14 +34,15 @@ func main() {
 }
 
 func digitsFromCommandLine(default_digits int) int {
-	if len(os.Args) > 2 || (len(os.Args) > 1 &&
-		(os.Args[1] == "-h" || os.Args[1] == "--help")) {
+	args := flag.Args()
+	if len(args) > 1 || (len(args) == 1 &&
+		(args[0] == "-h" || args[0] == "--help")) {
 		exitWithUsage()
 	}
-	if len(os.Args) > 1 {
-		if places, err := strconv.Atoi(os.Args[1]); err != nil {
+	if len(args) == 1 {
+		if places, err := strconv.Atoi(args[0]); err != nil {
 			fmt.Printf("%s: invalid number %s\n",
-				filepath.Base(os.Args[0]), os.Args[1])
+				filepath.Base(os.Args[0]), args[0])
 			exitWithUsage()
 		} else {
 			return places
@@ -41,9 +57,9 @@ func exitWithUsage() {
 }
 
 func π(digits int) *big.Int {
-	factor := big.NewInt(int64(digits))
+	precision := big.NewInt(int64(digits))
 	ten := big.NewInt(10)
-	unity_exponent := big.NewInt(0).Add(factor, ten)
+	unity_exponent := big.NewInt(0).Add(precision, ten)
 	unity := big.NewInt(0).Exp(ten, unity_exponent, nil)
 	left := big.NewInt(0).Mul(big.NewInt(4), arccot(big.NewInt(5), unity))
 	right := arccot(big.NewInt(239), unity)
@@ -53,19 +69,19 @@ func π(digits int) *big.Int {
 
 func arccot(x, unity *big.Int) *big.Int {
 	zero := big.NewInt(0)
-	minus_one := big.NewInt(-1)
-	x_squared := big.NewInt(0).Mul(x, x)
+	minus_x_squared := big.NewInt(0).Mul(x, x)
+	minus_x_squared.Neg(minus_x_squared)
 	coefficient := big.NewInt(1)
 	two := big.NewInt(2)
-	divisor := big.NewInt(0).Div(unity, x)
+	term := big.NewInt(0).Div(unity, x)
 	sum := big.NewInt(0)
-	factor := big.NewInt(0).Div(divisor, coefficient)
-	for factor.Cmp(zero) != 0 {
-		sum.Add(sum, factor)
+	divisor := big.NewInt(0)
+	for term.Cmp(zero) != 0 {
+		sum.Add(sum, term)
+		term.Mul(term, coefficient)
 		coefficient.Add(coefficient, two)
-		divisor.Mul(divisor, minus_one)
-		divisor.Div(divisor, x_squared)
-		factor.Div(divisor, coefficient)
+		divisor.Mul(minus_x_squared, coefficient)
+		term.Quo(term, divisor)
 	}
 	return sum
 }
